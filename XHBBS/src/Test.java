1d71761642b0c0eb60ff5d34ce24cf18dc3e905d
Administrator <Administrator@ZC7BV9ATZWZIN2C>
2015-09-10 17:54:53 +0800

    平板展示，回复主题，分页

---
+++ XHBBS/src/Test.java
@@ -0,0 +1,65 @@
+/*************************************************************************************************
+ * @版权所有 (C)2015,  星火工作室
+ * 
+ * @文件名称：Test.java
+ * @内容摘要：升级服务
+ * @当前版本： TODO
+ * @作        者： 李加蒙
+ * @完成日期：2015年9月10日 下午5:25:48
+ * @修改记录：
+ * @修改日期：2015年9月10日 下午5:25:48
+ * @版   本  号：
+ * @修   改  人：
+ * @修改内容：
+ ************************************************************************************************/
+import com.xinghuo.jdbc.JDBC;
+
+import java.sql.*;
+
+/**
+ * @文件名称：Test.java
+ */
+public class Test {
+	final static int PAGE_SIZE = 10;
+
+	public static void main(String[] args) {
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSetConunt = null;
+		try {
+			int totalPages = 0;
+			connection = JDBC.getConnection();
+			statement = JDBC.getStatement(connection);
+			resultSetConunt = JDBC.executeQuery(statement,
+					"select count(*) from article where pid = 0");
+			resultSetConunt.next();
+			int totalRecord = resultSetConunt.getInt(1);
+
+			while (resultSetConunt.next()) {
+				System.out.println("resultSetConunt");
+			}
+
+			totalPages = (totalRecord + PAGE_SIZE - 1) / PAGE_SIZE;
+
+			System.out.println(totalRecord + "  a " + totalPages);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSetConunt != null) {
+					resultSetConunt.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+
+			}
+		}
+	}
+}
